337598c1a2dbf3b35fa2f7a462373c37e86951eb
Matti Mattila <matti.mattila@leanware.fi>
2018-10-05 16:33:43 +0300

    Add main table component
    Add main table component which means the actual view of showing the record data

---
+++ risServer/recordinfosuperior-client/src/app/main-table-view/main-table-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-table-view',
+  templateUrl: './main-table-view.component.html',
+  styleUrls: ['./main-table-view.component.css']
+})
+export class MainTableViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
